e22d545812ad6be555f0e128a9ccdc322ed4d81a
Andrey <gryn87@gmail.com>
2017-07-08 15:33:49 +0300

    Update ir-monitor.php

--- ir-monitor.php
+++ ir-monitor.php
@@ -1,8 +1,16 @@
 #!/usr/bin/php5
-
 <?php
+
+// /dev/input/event1 - у cubieboard это I/O IR  и он должен быть инициализирован
+
 $dev="/dev/input/event1";
 $f=fopen($dev, 'rb');
+
+//
+//// Что бы понять, какой код отправляет та или иная кнопка твоего IR пульта, запусти
+//// cat /dev/input/event1 | hexdump
+//
+
 if($f){
     while (!feof($f)){
         $b=fread($f,32);  //каждая кнопка формирует данные по 64 бита, 32-нажата кнопка, еще 32 - отпустили
@@ -22,7 +30,7 @@ if($f){
             if ($key=="0001"){shell_exec("/home/cubie/proj/ir-vlc/volup.sh");} // громкость+
             break;
             case "0040":
-            if ($key=="0001"){shell_exec("/home/cubie/proj/ir-vlc/voldown.sh");} // громкость-
+            if ($key=="0001"){shell_exec("/home/cubie/proj/ir-vlc/voldown.sh");} // вообщем, shell_exec, ты понял.
             break;
         }
        
